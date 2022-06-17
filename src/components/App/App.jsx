6c3aa04a8cef7564d60aba23aa08b21b3971e0ab
Artyom <jyjyjyj3@gmail.com>
2022-06-17 23:58:02 +0300

    feature: create the appContext

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { usePopUpState, useAuthState } from '../../hooks';
 import { Header } from '../Header/Header';
 import { MainPage } from '../MainPage';
 import { NewsPage } from '../NewsPage';
 
 import styles from './App.module.scss';
 
+export const AppContext = React.createContext();
+
 export const App = () => {
+  const authPopUp = usePopUpState();
+  const auth = useAuthState();
+  console.log(auth, authPopUp);
+
   return (
-    <>
+    <AppContext.Provider
+      value={{
+        authPopUp,
+        auth,
+      }}
+    >
       <BrowserRouter>
         <Header />
         <main className={styles['page-wrapper']}>
@@ -18,6 +30,6 @@ export const App = () => {
           </Routes>
         </main>
       </BrowserRouter>
-    </>
+    </AppContext.Provider>
   );
 };
