3bdcda889ac04e53331f86d909ad0e38ebd158c8
Artyom <jyjyjyj3@gmail.com>
2022-06-18 21:30:45 +0300

    feature: use the AuthPopUp

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+import { AuthPopUp } from '../AuthPopUp';
 
 import styles from './MainPage.module.scss';
 
 export const MainPage = () => {
+  const { auth } = useContext(AppContext);
+
   return (
     <>
+      <AuthPopUp />
       <div className={styles['main-page']}>
-        <h1>привет гость</h1>
       </div>
     </>
   );
