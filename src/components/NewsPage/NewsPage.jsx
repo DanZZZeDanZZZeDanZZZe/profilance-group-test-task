0077a8a6377becb0749cccd5e90aac0056a4a6a7
Artyom <jyjyjyj3@gmail.com>
2022-06-19 15:12:48 +0300

    feature: add news filter

--- src/components/NewsPage/NewsPage.jsx
+++ src/components/NewsPage/NewsPage.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { usePopUpState } from '../../hooks';
 import { AppContext } from '../App';
+import { PopUp } from '../PopUp';
+
+import { throttle } from 'throttle-debounce';
 
 import styles from './NewsPage.module.scss';
 import commonStyles from '../../common-styles.module.scss';
-
-import { PopUp } from '../PopUp';
 
 const AddNewsContols = ({ addNewsHandler }) => {
   const popUp = usePopUpState();
@@ -63,19 +64,37 @@ const AddNewsContols = ({ addNewsHandler }) => {
 
 export const NewsPage = () => {
   const { auth, news } = useContext(AppContext);
+  const [filterString, setFilterString] = useState('');
 
   const showedNews =
     auth.isAdmin || auth.isUser ? news.allContent : news.acceptedContent;
 
+  const filteredShowedNews =
+    filterString !== ''
+      ? showedNews.filter((item) =>
+          item.title.toLowerCase().includes(filterString.trim('').toLowerCase())
+        )
+      : showedNews;
+
+  const filterInputOnChangeHandler = (e) => {
+    console.log(e.target.value);
+    setFilterString(e.target.value);
+  };
+
   return (
     <div className={styles['news-page']}>
+      <input
+        placeholder='Поиск по заголовку'
+        onChange={throttle(300, filterInputOnChangeHandler)}
+        className={`${commonStyles['project-input']} ${styles['news-page__filter-input']}`}
+      />
       <div className={styles['news-page__list']}>
-        {showedNews.map(({ id, title, text, date, isApproved }) => (
-          <div
+        {filteredShowedNews.map(({ id, title, text, date, isApproved }) => (
+          <article
             key={id}
             className={`${commonStyles['project-box']} ${styles['news-page__list-item']} ${styles['news-block']}`}
           >
-            <p className={styles['news-block__title']}>{title}</p>
+            <h2 className={styles['news-block__title']}>{title}</h2>
             <p className={styles['news-block__text']}>{text}</p>
             <p className={styles['news-block__date']}>{date}</p>
             {auth.isAdmin && (
@@ -98,7 +117,7 @@ export const NewsPage = () => {
                 </button>
               </div>
             )}
-          </div>
+          </article>
         ))}
       </div>
       {auth.isUser && <AddNewsContols addNewsHandler={news.add} />}
